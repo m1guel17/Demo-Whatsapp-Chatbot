383d9247829625fc426d8b09b4922785d34cd9c0
ignxm <ignx.mfs@gmail.com>
2024-09-03 22:25:52 -0500

    add_main_test1

--- app/trunk/main.py
+++ app/trunk/main.py
@@ -1,13 +1,9 @@
 from app.msgs.api_wsp import send_response
 from app.format.json import json
-from app.business_logic import get_row, get_branch
 
 def init_flow(txt, number):
     txt = txt.lower()
-    
-    user_state = get_row(number)
-    branch_state = get_branch(number)
 
     if txt == "hola":
-        data = json(number, "🤖 Hola, ¿Cómo estas? Bienvenido. " + user_state + branch_state)
+        data = json(number, "🤖 Hola, ¿Cómo estas? Bienvenido. ")
         send_response(data)
