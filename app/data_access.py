f55d9b658e84d0fdfc6db80cb351dc55d1968bf9
ignxm <ignx.mfs@gmail.com>
2024-09-03 22:39:08 -0500

    add_main_test4

--- app/data_access.py
+++ app/data_access.py
@@ -3,13 +3,13 @@ from . import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(100), nullable=True)
+    full_name = db.Column(db.TEXT, nullable=True)
     cellphone = db.Column(db.TEXT, unique=True, nullable=True)
     id_number = db.Column(db.Integer, unique=True, nullable=True)
     address = db.Column(db.TEXT, nullable=True)
     address_reference = db.Column(db.TEXT, nullable=True)
-    email = db.Column(db.String(100), unique=True, nullable=True)
-    status = db.Column(db.String(10), nullable=True)
+    email = db.Column(db.TEXT, unique=True, nullable=True)
+    status = db.Column(db.TEXT, nullable=True)
     registrationDate = db.Column(db.DateTime, default = datetime.utcnow)
 
 
@@ -18,7 +18,7 @@ class Conversations(db.Model):
     cellphone = db.Column(db.TEXT, unique=True, nullable=False)
     msg = db.Column(db.TEXT, nullable=True)
     ex_funct = db.Column(db.TEXT, nullable=True)
-    branch = db.Column(db.String(10), nullable=True)
+    branch = db.Column(db.TEXT, nullable=True)
     status = db.Column(db.TEXT, nullable=True)
     date_time = db.Column(db.DateTime, default = datetime.utcnow)
     
